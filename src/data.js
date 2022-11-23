cb48b8e83e4ec2eceea5e0d0704ad368ed56417c
IdanKfir <zunzer244@gmail.com>
2022-11-23 05:20:23 +0200

    initial commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: 'Samsung Galaxy S7',
+    price: 599.99,
+    img: 'https://www.kindpng.com/picc/m/469-4699711_samsung-galaxy-s7-czarny-hd-png-download.png',
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: 'google pixel ',
+    price: 499.99,
+    img: 'https://purepng.com/public/uploads/large/purepng.com-google-pixel-1smartphoneandroidgooglephoneapplication-211519340822fzn7t.png',
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: 'Xiaomi Redmi Note 2',
+    price: 699.99,
+    img: 'https://www.pngfind.com/pngs/m/185-1856689_xiaomi-mi-note-2-hd-png-download.png',
+    amount: 1,
+  },
+];
